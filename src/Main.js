12b0326c193ff8dddf3b258a5a5066f7e3852156
lucamezzalira <mezzalab@gmail.com>
2016-03-29 00:23:38 +0100

    implemented dynamic load and styling

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,53 @@
 import Rx from 'rx';
+import moment from 'moment';
 import Cycle from '@cycle/core';
-import {makeDOMDriver, option, ul, li, select, div} from '@cycle/dom';
+import {makeDOMDriver, option, ul, li, h2, select, div, p, span} from '@cycle/dom';
 import {makeHTTPDriver} from '@cycle/http';
 
-function body(results){
-    console.log("results", results.text);
+function getTrainData(data){
+    return li(".train", [
+        div(".train-data",
+        [
+            p(".stationName", [span("station: "), data.stationName]),
+            p(".platform", [span("platform: "), data.platformName]),
+            p("current-location", [span("current location: "), data.currentLocation]),
+            p(".arrival-time: ", [span("expected arrival time: "), moment(new Date(data.expectedArrival)).format("HH:MM - Do MMM YYYY")])
+        ]
+    )])
+}
+
+function getDestinationStation(data){
+    return div(".destination-station", [
+        h2(data.destination),
+        ul(".destination-trains-available", data.trains.map(item => getTrainData(item)))
+    ]);
+}
+
+function renderTrainsData(data){
+    return div(".all-stations", data.map(item => getDestinationStation(item)));
+}
+
+function normaliseData(data){
+    let finalData = [];
+
+    data.sort((a,b) => {
+        if(a.destinationName > b.destinationName)
+            return 1
+        if(a.destinationName < b.destinationName)
+            return -1
+            
+        return 0
+    }).map((item, i, arr) => {
+        if(finalData.length === 0 || item.destinationName !== finalData[finalData.length-1].destination)
+            finalData.push({destination: item.destinationName, trains:[]});
+        
+        finalData[finalData.length-1].trains.push(item);
+    });
+                
+   return finalData
+}
+
+function getBody(results){
     return div(".container", [
                 select("#lines", [
                     option({ value: 'district' }, ["District line"]),
@@ -15,26 +58,41 @@ function body(results){
                     option({ value: 'piccadilly' }, ["Piccadilly line"]),
                     option({ value: 'victoria' }, ["Victora line"]),
                 ]),
-                ul(".tube-results")
+                renderTrainsData(normaliseData(results))
             ])           
 }
 
 function main(drivers) {
-    //let API_URL = 'https://api.tfl.gov.uk/line/district/arrivals?app_id=a2420191&app_key=b81115a21d9e11449d8fffd165644709';
-    let API_URL = "data.json";
-    let request$ = Rx.Observable.just({
-        url: API_URL
-    });
+   // let API_URL = "data.json"; //for local data
+   const API_URL = "https://api.tfl.gov.uk/line/";
+
+    let linesRequest$ = drivers.DOM.select("#lines").events("change")
+                                .startWith({target: 
+                                    {value: "district"}
+                                })
+                                .map(evt => {
+                                    let selectedLine = evt.target.value;
+                                    
+                                    return {
+                                        //url: API_URL
+                                        url: API_URL + selectedLine + "/arrivals?app_id=a2420191&app_key=b81115a21d9e11449d8fffd165644709"
+                                    }
+                                });
     let vtree$ = drivers.HTTP
-                .filter(res => res.request.url === API_URL)
+                .filter(res => res.request.url.indexOf(API_URL) === 0)
                 .mergeAll()
-                .startWith("loading...")
                 .map(res => {
-                    return body(res);
+                    let body = div();
+                    let data = res.text;
+                    if(data)
+                        body = getBody(JSON.parse(data));
+                    
+                    return body;
                 });
+
     return {
         DOM: vtree$,
-        HTTP: request$
+        HTTP: linesRequest$
     };
     
 }
